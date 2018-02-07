c3fee5906199eef1c46f1e64a1269c113a42875f
Jan Luth <ermelo1983@gmail.com>
2018-02-07 14:16:17 +0100

    busy with getting and calculating that shifts doesnt get bigger than the employee contracthours

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -33,23 +33,36 @@ class ShiftsController < ApplicationController
     #this has to be something like Location.shifts.all and/or Unit.shifts.all although i think this is covered in the first
     days.each do |date|
       shift_types.each do |shift_type|
-        employee = get_employee(shift_type)
+        employee = get_location_or_unit(shift_type, employee)
         shift_type.quantity_per_day.times do
-          Shift.create(date: date.to_date, month: date.strftime("%B"), shift_type_id: shift_type.id, employee_id: rand(employee[0] .. (employee.count-1)))
+          Shift.create(date: date.to_date, month: date.strftime("%B"), shift_type_id: shift_type.id, employee_id: employee)
+          employee.made_hours +1
         end
       end
     end
     redirect_to shifts_path,  notice:"Successfully created shift for a month"
   end
 
-  def get_employee(shift_type)
+  def get_location_or_unit(shift_type, employee)
     if shift_type.per_location?
-      employee = Employee.where(unit_id: Unit.where(location_id: shift_type.location_id)).ids
+      employee = get_employee(shift_type, employee)
     elsif shift_type.per_unit?
       unit = Unit.where(location_id: shift_type.location_id)
       employee = Employee.where(unit_id: unit.ids).ids
     end
   end
+
+  def get_employee(shift_type, employee)
+    employees = Employee.where(unit_id: Unit.where(location_id: shift_type.location_id))
+    employees.each do |employee|
+      given_hours = employee.contract_hours
+      made_hours = 0
+      # made_hours + (shift.end_time - shift.start_time)
+      # if made_hours == given_hours || made_hours => given_hours current employee cant be given to create_this_month_shift
+    end
+  end
+
+
 
   def destroy
   end
